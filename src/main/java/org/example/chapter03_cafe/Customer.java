69c671d3d85a26833ea30a4354b885e23e6723f9
A2N2 <129242580+atoonatoo@users.noreply.github.com>
2024-01-31 00:28:35 +0900

    주문한 커피가 null값이 뜨는 상황 list에 재대로 저장이 안되는 것 같음 list컬렉션에 좀 더 공부해볼필요있음 더불어 바리스타와 내 소지금도 출력이 안된다 초기화관련문제로 예상

--- src/main/java/org/example/chapter03_cafe/Customer.java
+++ src/main/java/org/example/chapter03_cafe/Customer.java
@@ -6,6 +6,8 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Customer {
+    Scanner sc = new Scanner(System.in); // 스캐너
+
     private String customerName = "제임스";
     private int customerMoney = 10000;
 
@@ -20,7 +22,6 @@ public class Customer {
     //메뉴판 보기
     public void orderCoffee(CoffeeMenu coffeeMenu, Barista barista) {
         coffeeMenu.menuChoose(); // 메뉴판 보기
-        Scanner sc = new Scanner(System.in); // 스캐너
         String userInput = sc.nextLine(); // 입력값 넣기
         CoffeeType coffeeType = CoffeeType.valueOf(userInput); //입력값과 일치하는 이넘클래스 찾기
         CoffeeInfomation coffeeInfomation; //인터페이스 초기화
@@ -37,8 +38,10 @@ public class Customer {
         }
         pay(coffeeInfomation.getPrice());
         baristaMoney(coffeeInfomation.getPrice(), barista);
+        takeCoffee(coffeeInfomation.getCoffeeName());
         System.out.println(coffeeInfomation.getCoffeeName() + coffeeInfomation.getPrice());
     }
+
 
     private int baristaMoney(int price, Barista barista) {
 
@@ -53,4 +56,23 @@ public class Customer {
     public void remainingMoney() {
         System.out.println("내 소지금 : " + customerMoney + " 원");
     }
+
+
+    public void takeCoffee(String takeCoffee) {
+
+        System.out.println("주문한 커피 " + takeCoffee + "를 마시겠습니까?");
+        System.out.println("1. 마신다 | 2. 안마신다.");
+
+        int drink = sc.nextInt();
+        if (drink == 1) {
+            System.out.println("아이 맛있다~ " + takeCoffee + " 맛이네~ + 개수");
+            return ;
+        }
+        if (drink == 2 ){
+            System.out.println("나중에 마시자..");
+            return;
+        }
+    }
+
+
 }
